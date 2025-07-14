5da314a0bd9542a683e17fa026755acd90e38e87
Cyrill Berg <cyrill.berg@bwi.de>
2025-07-14 21:09:32 +0200

    fix: fixed flake and added some more packages
    Signed-off-by: Cyrill Berg <cyrill.berg@bwi.de>

--- flake.nix
+++ flake.nix
@@ -1,28 +1,34 @@
 {
-  description = "Project with Go and Task";
+  description = "Development shell & build Starlord";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";  # or stable
+    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        isDarwin = builtins.match ".*-darwin.*" system != null;
       in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.go_1_24
-            pkgs.go-task
-            pkgs.minikube
-            pkgs.kubernetes-helm
-          ];
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; (
+            [
+              git
+              kind
+              kubectl
+              k9s
+              go-task
+              go_1_23
+              golint
+            ]
+          );
           shellHook = ''
             export IN_NIX_SHELL=1
             exec zsh
           '';
         };
-      }
-    );
+
+      });
 }
